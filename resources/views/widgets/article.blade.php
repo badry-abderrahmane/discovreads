bbc8a961ce96fb6897adc283b2f4d17e3e13ff7e
Badry Abderrahmane <badry-abderrahmane@git.com>
2018-08-15 23:22:11 +0100

    Styled featured article

--- resources/views/widgets/article.blade.php
+++ resources/views/widgets/article.blade.php
@@ -5,46 +5,43 @@
 	<div class="section-title">
 		<h2><span>Featured</span></h2>
 	</div>
-	<div class="card-columns listfeaturedtag">
-		@forelse($articles as $article)
+	<div class="row card-columns listrecent">
+    <!-- begin post -->
+    @forelse($articles as $article)
 			@if($article->featured)
-			<!-- begin post -->
-			<div class="card">
-				<div class="row">
-					@if($article->page_image)
-					<div class="col-md-5 wrapthumbnail">
-						<a href="{{ url($article->slug) }}">
-							<div class="thumbnail" style="background-image:url({{ $article->page_image }});">
-							</div>
-						</a>
-					</div>
-					@endif
-					<div class="col-md-7">
-						<div class="card-block">
-							<h2 class="card-title"><a href="{{ url($article->slug) }}">{{ $article->title }}</a></h2>
-							<h4 class="card-text">{{ $article->meta_description }}</h4>
-							<div class="metafooter">
-								<div class="wrapfooter">
-									<span class="meta-footer-thumb">
-									<a href="author.html"><img class="author-thumb" src="https://www.gravatar.com/avatar/e56154546cf4be74e393c62d1ae9f9d4?s=250&amp;d=mm&amp;r=x" alt="Sal"></a>
-									</span>
-									<span class="author-meta">
-									<span class="post-name"><a href="author.html">{{ $article->user->name or 'null' }}</a></span><br/>
-									<span class="post-date">{{ $article->published_at->diffForHumans() }}</span><span class="dot"></span><span class="post-read">6 min read</span>
-									</span>
-									<span class="post-read-more"><a href="post.html" title="Read Story"><svg class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25"><path d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z" fill-rule="evenodd"></path></svg></a></span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!-- end post -->
+    		<div class="col-lg-6">
+		      <div class="card">
+		        @if($article->page_image)
+		        <a href="post.html">
+		          <img class="img-fluid" src="{{ $article->page_image }}" alt="">
+		        </a>
+		        @endif
+		        <div class="card-block">
+		          <h2 class="card-title"><a href="{{ url($article->slug) }}">{{ $article->title }}</a></h2>
+		          <h4 class="card-text">{{ $article->meta_description }}</h4>
+		          <div class="metafooter">
+		            <div class="wrapfooter">
+		              <span class="meta-footer-thumb">
+		              <a href="author.html"><img class="author-thumb" src="https://www.gravatar.com/avatar/e56154546cf4be74e393c62d1ae9f9d4?s=250&amp;d=mm&amp;r=x" alt="Sal"></a>
+		              </span>
+		              <span class="author-meta">
+		              <span class="post-name"><a href="author.html">{{ $article->user->name or 'null' }}</a></span><br/>
+		              <span class="post-date">{{ $article->published_at->diffForHumans() }}</span><span class="dot"></span><span class="post-read">{{ $article->view_count }} reads</span>
+		              </span>
+		              <span class="post-read-more"><a href="post.html" title="Read Story"><svg class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25"><path d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z" fill-rule="evenodd"></path></svg></a></span>
+		            </div>
+		          </div>
+		        </div>
+		      </div>
+		    </div>
 			@endif
-		@empty
+    @empty
         <h3 class="text-center">{{ lang('Nothing') }}</h3>
     @endforelse
-	</div>
+    <!-- end post -->
+
+  </div>
+
 	</section>
 	<!-- End Featured
 	================================================== -->
